0048822352cd3a10294e8ba712cff7a6d33531ff
FabioPinheiro <fabiomgpinheiro@gmail.com>
2021-07-12 00:36:13 +0100

    New Rhythmic Gymnastics Pavilion
    Add more function KhepriUtils

--- modules/03-controller/src/main/scala/app/fmgp/geo/Syntax.scala
+++ modules/03-controller/src/main/scala/app/fmgp/geo/Syntax.scala
@@ -11,14 +11,41 @@ object Syntax extends Syntax {
   }
 }
 
-trait Syntax extends BaseSyntax with KhepriSolidPrimitives
+trait Syntax extends BaseSyntax with KhepriUtils with KhepriSolidPrimitives
+
+/** Khepri's defined functions
+  * @see [[https://github.com/aptmcl/Khepri.jl/blob/master/src/Utils.jl]]
+  */
+trait KhepriUtils {
+  def division(t0: Double, t1: Double, n: Int, include_last: Boolean = true): Seq[Double] =
+    val step = (t1 - t0) / n
+    (if (include_last)(0.to(n)) else (0.until(n))).map(e => t0 + step * e)
+
+  def map_division[T](f: (Double) => T, t0: Double, t1: Double, n: Int, include_last: Boolean = true): Seq[T] =
+    division(t0, t1, n, include_last).map(f)
+  def map_division[T](
+      f: (Double, Double) => T,
+      u0: Double,
+      u1: Double,
+      nu: Int,
+      v0: Double,
+      v1: Double,
+      nv: Int,
+      include_last: Boolean
+  ): Seq[Seq[T]] =
+    division(u0, u1, nu, include_last)
+      .map(u => division(v0, v1, nv, include_last).map(v => f(u, v)))
+}
+
 trait BaseSyntax {
   var wireframeMode: Boolean = false
   def addShape[T <: Shape](t: T): T = addShape(t = t, wireframeMode = wireframeMode)
   def addShape[T <: Shape](t: T, wireframeMode: Boolean): T
   def clear: Unit = { wireframeMode = false }
 
+  def u0() = XYZ.origin
   def xyz(x: Double = 0, y: Double = 0, z: Double = 0): XYZ = XYZ(x, y, z)
+  def vxyz(x: Double = 0, y: Double = 0, z: Double = 0): Vec = Vec(x, y, z)
   def pol = polar _
   def polar(module: Double, argument: Double): XYZ = Polar(rho = argument, phi = module).toXYZ
   def cyl = cylindrical _
