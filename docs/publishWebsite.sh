06d758854f4c523d694818297b345879bc532072
FabioPinheiro <fabiomgpinheiro@gmail.com>
2021-09-03 00:23:38 +0100

    webapp publish & add CNAME

--- docs/publishWebsite.sh
+++ docs/publishWebsite.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+scriptdir="$(dirname "$0")"
 
-(cd .. ; sbt -mem 2048 -J-Xmx5120m "clean; fastOptJS::webpack")
-cp ../modules/03-webapp/target/scala-3.0.0/scalajs-bundler/main/fmgp-geometry-webapp-fastopt-bundle.js . &&
+#(cd "$scriptdir/.." ; sbt -mem 2048 -J-Xmx5120m "clean; fastOptJS::webpack")
+[[ -z $(git status -uall --porcelain) ]] && {
+  cp "$scriptdir/../modules/04-webapp/target/scala-3.0.0/scalajs-bundler/main/fmgp-geometry-webapp-fastopt-bundle.js" "$scriptdir" &&
   git branch gh-pages &&
   git checkout gh-pages &&
-  git add --force ./fmgp-geometry-webapp-fastopt-bundle.js &&
+  #git pull &&
+  git add --force "$scriptdir/fmgp-geometry-webapp-fastopt-bundle.js" &&
   git commit -m "gh-pages update at $(date '+%Y%m%d-%H%M%S')" &&
   git push --set-upstream origin gh-pages -f &&
-  git checkout master && git branch -d gh-pages # delete branch locally
+  git checkout master &&
+  git branch -d gh-pages # delete branch locally
+} || echo "This branch is NOT clean."
 
 #Now check https://fabiopinheiro.github.io/fmgp-generative-design/
 #aka https://geo.fmgp.app/
