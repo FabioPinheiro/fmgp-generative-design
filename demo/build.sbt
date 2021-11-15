cb8ff843fdf9101b6dac87088b294d0e74ba914d
FabioPinheiro <fabiomgpinheiro@gmail.com>
2021-11-15 01:03:40 +0000

    Demo

---
+++ demo/build.sbt
@@ -0,0 +1,11 @@
+lazy val root = project
+  .in(file("."))
+  .settings(
+    name := "demo",
+    description := "Example fmgp project that compiles using Scala 3",
+    version := "0.1.0",
+    scalaVersion := "3.1.0",
+    libraryDependencies += "com.softwaremill.sttp.client3" %% "core" % "3.3.14",
+    libraryDependencies += "app.fmgp" %% "fmgp-geometry-syntax" % "0.0.0+140-6ddceb07-SNAPSHOT",
+    //libraryDependencies += "app.fmgp" %% "controller" % "0.0.0+140-6ddceb07-SNAPSHOT",
+  )
---
+++ demo/src/main/scala/Main.scala
@@ -0,0 +1,54 @@
+import sttp.client3._
+import io.circe._, io.circe.syntax._ //, io.circe.parser._
+
+import zio._
+import scala.math._
+import fmgp.geo._
+import fmgp.geo.EncoderDecoder.{given}
+import fmgp.dsl._
+
+import IsenbergSchoolOfManagementHubExample._
+
+@main
+def Main(args: String*): Unit =
+  println("SingleRequest Start")
+
+  val requestClean = basicRequest
+    .post(uri"http://localhost:8888/add")
+    .body(World.w3DEmpty.asJson.noSpaces)
+
+  val requestAdd = basicRequest
+    .post(uri"http://localhost:8888/add")
+    .body {
+      val shapes = zio.Runtime.global.unsafeRun(program.inject(DslLive.layer))
+      World.addition(shapes).asWorld.asJson.noSpaces
+    }
+
+  val backend = HttpURLConnectionBackend()
+  val responseClean = requestClean.send(backend)
+  val responseAdd = requestAdd.send(backend)
+  println("SingleRequest End")
+end Main
+
+def program: ZIO[Has[Dsl], Nothing, fmgp.geo.Shape] =
+  for {
+    _ <- ZIO.unit
+    isenberg = Isenberg(
+      XYZ.origin,
+      ri = 5,
+      re = 10,
+      alfa_init = 0,
+      alfa_proj = Pi,
+      alfa_end = 3d / 2 * Pi,
+      n = 50,
+      slabThickness = 0.02,
+    )
+    height = 1.5
+    floors = ShapeSeq((1 to 5).map(f => isenberg.slabRoof(height * f)))
+    beams = isenberg.beams(height)
+    shapes = Shape(
+      isenberg.slabFloor,
+      floors,
+      beams
+    ).transformWith(Matrix.rotate(-Pi / 2, Vec(x = 1)))
+  } yield (shapes)
